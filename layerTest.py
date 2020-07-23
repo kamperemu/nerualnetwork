3ee205fe9f43d538768a05404b4376894c62e10e
kamperemu <34961229+kamperemu@users.noreply.github.com>
2020-07-23 21:29:39 +0300

    Delete layerTest.py

--- layerTest.py
+++
@@ -1,7 +0,0 @@
-from layer import neuralLayer
-
-inputs = [[0,0],[0,1],[1,0],[1,1]]
-outputs = [[0,0],[1,0],[1,0],[1,1]]
-
-layer = neuralLayer(2,2,"sigmoid")
-layer.backProp(layer.think(inputs[0]))
